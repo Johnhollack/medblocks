25a3ead4cf1911f3002ca6d2b5f1333a267a4231
johnhollack <adejumolekan@gmail.com>
2021-07-24 19:19:53 +0100

    init

---
+++ pages/patients.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import { LockClosedIcon } from '@heroicons/react/solid'
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Medblocks - Patients</title>
+        <meta name="Medblocks" content="HealthCare is a healthcare management app for indians." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+
+     </div>
+  )
+}
